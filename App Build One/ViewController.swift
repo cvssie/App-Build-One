3bcf7cc85a3462738d896fc240144c98796442c4
Cassandra K <ckk30@hawaii.edu>
2023-10-05 13:36:03 -1000

    completed

--- App Build One/ViewController.swift
+++ App Build One/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var uhwo: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        uhwo.text = "University of Hawai'i West Oahu"
+        
+        self.view.backgroundColor = UIColor.systemGray2
     }
-
-
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func aboutUHWO(_ sender: Any) {
+        textBox.text = "UH West O‘ahu is a four-year, comprehensive university with an emphasis on baccalaureate education founded in the liberal arts, serving professional, career-related, and applied fields, based on State and regional needs"
+    }
+    
+    @IBAction func aboutUCM(_ sender: Any) {
+        textBox.text = "Students who chose to study creative media will receive an innovative educational experience that weaves art and design, narrative, information science, and technologies necessary for a broad range of careers in government and non-government organizations, educational institutions, technological firms and private firms and agencies."
+        
+    }
+    
+    
+    @IBOutlet weak var textBox: UITextView!
 }
 
--- App Build One/secondViewController.swift
+++ App Build One/secondViewController.swift
@@ -8,22 +8,48 @@
 import UIKit
 
 class secondViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var blank: UILabel!
+    
+    
+    class secondViewController: UIViewController {
+        
+       
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            // Do any additional setup after
+            
+            
+        }
+        
+        
+       
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        var number1 = 23
+        var number2 = 16
+       
+        
+        @IBAction func calculate(_ sender: Any) {
+          
+           
+            
+                
+    
+            
+            
+            
+        }
+        
+        
+        
     }
-    */
-
 }
